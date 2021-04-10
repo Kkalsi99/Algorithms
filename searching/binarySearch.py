7e01cd3e9f04bbdb178d912ec52194970eb730bd
Kkalsi <kalsi.kashish@gmail.com>
2021-04-10 16:53:20 +0530

    Updated BS

--- searching/binarySearch.py
+++ searching/binarySearch.py
@@ -5,14 +5,14 @@ def binarySearch(arr, element):
         if(arr[mid] == element):
             return mid
         elif(arr[mid] < element):
-            mid = int((mid+len(arr)-1)/2)
+            mid = int((mid+len(arr))/2)
         elif(arr[mid] > element):
             mid = int((mid-1)/2)
 
 
 def main():
-    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    element = 7
+    arr = [1, 2, 3, 4]
+    element = 1
     print(binarySearch(arr, element))
 
 
